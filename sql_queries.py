deeb87213f320dc7c783044b7d64f5692f8e1e15
Ove <osundal83@gmail.com>
2019-05-07 22:11:20 +0200

    add songplays and users table

--- sql_queries.py
+++ sql_queries.py
@@ -7,13 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -24,9 +24,29 @@ staging_songs_table_create = ("""
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays
+(
+songplay_id int IDENTITY(0,1) PRIMARY KEY,
+start_time timestamp NOT NULL,
+user_id int  NOT NULL,
+level varchar(255) NOT NULL,
+song_id varchar(255) NOT NULL,
+artist_id varchar(255) NOT NULL,
+session_id int NOT NULL,
+location varchar(255) NOT NULL,
+user_agent varchar(255) NOT NULL
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users
+(
+user_id int PRIMARY KEY,
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+gender char NOT NULL,
+level varchar(255) NOT NULL
+);
 """)
 
 song_table_create = ("""
@@ -65,7 +85,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, staging_events_table_create, staging_songs_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
