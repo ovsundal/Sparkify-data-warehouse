3f31d193a8cf770107b3f3fde33312090c698b8c
Ove <osundal83@gmail.com>
2019-05-07 21:32:11 +0200

    add function for opening cluster endpoint

--- setup_redshift_cluster.py
+++ setup_redshift_cluster.py
@@ -4,12 +4,12 @@ import boto3
 
 
 def create_clients(KEY, SECRET, REGION):
-    iam = boto3.resource('iam', aws_access_key_id=KEY,
+    iam = boto3.client('iam', aws_access_key_id=KEY,
                          aws_secret_access_key=SECRET,
                          region_name=REGION)
 
     ec2 = boto3.resource('ec2',
-                         region_name="us-west-2",
+                         region_name=REGION,
                          aws_access_key_id=KEY,
                          aws_secret_access_key=SECRET
                          )
@@ -19,7 +19,7 @@ def create_clients(KEY, SECRET, REGION):
 
 def create_iam_role(iam, DWH_IAM_ROLE_NAME):
     """
-    Creates an iam role (add administrator access from inside AWS)
+    Creates an iam role (add administrator access from inside AWS), adds policy and returns the ARN
     """
     try:
         print('creating IAM role...')
@@ -33,37 +33,16 @@ def create_iam_role(iam, DWH_IAM_ROLE_NAME):
                                 'Principal': {'Service': 'redshift.amazonaws.com'}}],
                  'Version': '2012-10-17'})
         )
-        roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
-        print(roleArn)
+
+        iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
+                               PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+                               )['ResponseMetadata']['HTTPStatusCode']
     except Exception as e:
         print(e)
 
-
-def open_cluster_endpoint(ec2, myClusterProps, DWH_PORT):
-    """
-    Open an incoming TCP port to access the cluster endpoint
-    """
-    try:
-        print('Open incoming TCP port to access cluster endpoint...')
-        vpc = ec2.Vpc(id=myClusterProps['VpcId'])
-        print(vpc)
-
-        defaultSg = list(vpc.security_groups.all())
-        print(defaultSg)
-
-        print(defaultSg)
-        defaultSg.authorize_ingress(
-            GroupName=defaultSg.group_name,
-            CidrIp='0.0.0.0/0',
-            IpProtocol='TCP',
-            FromPort=int(DWH_PORT),
-            ToPort=int(DWH_PORT)
-        )
-    except Exception as e:
-        print(e)
 
 def create_cluster(REGION, KEY, SECRET, DWH_CLUSTER_TYPE, DWH_NODE_TYPE, DWH_NUM_NODES, DWH_DB,
-                   DWH_CLUSTER_IDENTIFIER, DWH_DB_USER, DWH_DB_PASSWORD):
+                   DWH_CLUSTER_IDENTIFIER, DWH_DB_USER, DWH_DB_PASSWORD, roleArn):
     """
     Creates a redshift cluster based on params in dwh.cfg
     """
@@ -85,12 +64,33 @@ def create_cluster(REGION, KEY, SECRET, DWH_CLUSTER_TYPE, DWH_NODE_TYPE, DWH_NUM
             DBName=DWH_DB,
             ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
             MasterUsername=DWH_DB_USER,
-            MasterUserPassword=DWH_DB_PASSWORD
+            MasterUserPassword=DWH_DB_PASSWORD,
+
+            # Roles (for s3 access)
+            IamRoles=[roleArn]
         )
-        return redshift
     except :
         print('Error, could not create cluster')
 
+
+def open_cluster_endpoint(ec2, myClusterProps, DWH_PORT):
+    """
+    Open an incoming TCP port to access the cluster endpoint
+    """
+    try:
+        print('Open incoming TCP port to access cluster endpoint...')
+        vpc = ec2.Vpc(id=myClusterProps['VpcId'])
+        defaultSg = list(vpc.security_groups.all())[0]
+
+        defaultSg.authorize_ingress(
+            GroupName=defaultSg.group_name,
+            CidrIp='0.0.0.0/0',
+            IpProtocol='TCP',
+            FromPort=int(DWH_PORT),
+            ToPort=int(DWH_PORT)
+        )
+    except Exception as e:
+        print(e)
 
 def main():
     config = configparser.ConfigParser()
@@ -109,33 +109,30 @@ def main():
     DWH_DB_PASSWORD = config.get('DWH', 'DWH_DB_PASSWORD')
     REGION = config.get('CLUSTER', 'REGION')
     DWH_IAM_ROLE_NAME = config.get("DWH", "DWH_IAM_ROLE_NAME")
-    DWH_PORT = config.get("DWH", "DWH_PORT")
+
 
     # get clients
     iam, ec2 = create_clients(KEY, SECRET, REGION)
 
-    # create iam role
+    # create iam role and get roleArn
     create_iam_role(iam, DWH_IAM_ROLE_NAME)
+    roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
 
     # create cluster
-    # create_cluster(REGION, KEY, SECRET, DWH_CLUSTER_TYPE, DWH_NODE_TYPE, DWH_NUM_NODES, DWH_DB,
-    #                DWH_CLUSTER_IDENTIFIER, DWH_DB_USER, DWH_DB_PASSWORD)
-    
-    # describe cluster
+    create_cluster(REGION, KEY, SECRET, DWH_CLUSTER_TYPE, DWH_NODE_TYPE, DWH_NUM_NODES, DWH_DB,
+                   DWH_CLUSTER_IDENTIFIER, DWH_DB_USER, DWH_DB_PASSWORD, roleArn)
+
+    # ONLY RUN THIS ONCE THE CLUSTER IS AVAILABLE
+    # open an incoming TCP port to access the cluster endpoint
+
+    DWH_PORT = config.get("DWH", "DWH_PORT")
     redshift = boto3.client('redshift',
                             region_name=REGION,
                             aws_access_key_id=KEY,
                             aws_secret_access_key=SECRET)
+
     myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
-
     open_cluster_endpoint(ec2, myClusterProps, DWH_PORT)
-
-
-
-
-    # TODO: create iam role, setup policy etc
-    # https://classroom.udacity.com/nanodegrees/nd027/parts/69a25b76-3ebd-4b72-b7cb-03d82da12844/modules/445568fc-578d-4d3e-ab9c-2d186728ab22/lessons/21d59f40-6033-40b5-81a2-4a3211d9f46e/concepts/2c454710-4a84-4c4f-9aeb-caa76f816466#
-    # https://classroom.udacity.com/nanodegrees/nd027/parts/69a25b76-3ebd-4b72-b7cb-03d82da12844/modules/445568fc-578d-4d3e-ab9c-2d186728ab22/lessons/21d59f40-6033-40b5-81a2-4a3211d9f46e/concepts/7a66583b-36de-4516-8cf8-2962e7b45c43
 
 
 if __name__ == "__main__":
