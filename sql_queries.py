28c6db6d8a01f172c92cbe1c0beba1f1c93c9b24
Ove <osundal83@gmail.com>
2019-05-15 23:03:15 +0200

    change table columns to allow null values

--- sql_queries.py
+++ sql_queries.py
@@ -1,6 +1,7 @@
 import configparser
 
 
+
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
@@ -20,40 +21,40 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 staging_events_table_create= ("""
 CREATE TABLE IF NOT EXISTS staging_events
 (
-    artist VARCHAR(255) NULL,
-    auth VARCHAR(255) NOT NULL,
-    firstName VARCHAR(255) NOT NULL,
-    gender CHAR NOT NULL,
-    itemInSession INT NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    length FLOAT NOT NULL,
-    level VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    method VARCHAR(255) NOT NULL,
-    page VARCHAR(255) NOT NULL,
-    registration FLOAT NOT NULL,
-    sessionId INT NOT NULL,
-    song VARCHAR(255) NULL,
-    status INT NOT NULL,
-    ts timestamp NOT NULL,
-    userAgent VARCHAR(255) NOT NULL,
-    userId INT NOT NULL
+    artist VARCHAR(255),
+    auth VARCHAR(255),
+    firstName VARCHAR(255),
+    gender CHAR,
+    itemInSession INT,
+    lastName VARCHAR(255),
+    length FLOAT,
+    level VARCHAR(255),
+    location VARCHAR(255),
+    method VARCHAR(255),
+    page VARCHAR(255),
+    registration FLOAT,
+    sessionId INT,
+    song VARCHAR(255),
+    status INT,
+    ts BIGINT,
+    userAgent VARCHAR(255),
+    userId INT
 )
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs
 (
-    num_songs INT NOT NULL,
-    artist_id VARCHAR(255) NOT NULL,
-    artist_latitude VARCHAR(255) NULL,
-    artist_longitude VARCHAR(255) NULL,
-    artist_location VARCHAR(255) NULL,
-    artist_name VARCHAR(255) NULL,
-    song_id VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    duration FLOAT NOT NULL,
-    year INT NOT NULL
+    num_songs INT,
+    artist_id VARCHAR(255),
+    artist_latitude VARCHAR(255),
+    artist_longitude VARCHAR(255),
+    artist_location VARCHAR(255),
+    artist_name VARCHAR(255),
+    song_id VARCHAR(255),
+    title VARCHAR(255),
+    duration FLOAT,
+    year INT
 )
 """)
 
@@ -61,47 +62,47 @@ songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS fact_songplays
 (
 songplay_id int IDENTITY(0,1) PRIMARY KEY,
-start_time timestamp NOT NULL,
-user_id int  NOT NULL,
-level varchar(255) NOT NULL,
-song_id varchar(255) NOT NULL,
-artist_id varchar(255) NOT NULL,
-session_id int NOT NULL,
-location varchar(255) NOT NULL,
-user_agent varchar(255) NOT NULL
+start_time timestamp,
+user_id int,
+level varchar(255),
+song_id varchar(255),
+artist_id varchar(255),
+session_id int,
+location varchar(255),
+user_agent varchar(255)
 );
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS dim_users
 (
-user_id int PRIMARY KEY,
-first_name varchar(255) NOT NULL,
-last_name varchar(255) NOT NULL,
-gender char NOT NULL,
-level varchar(255) NOT NULL
+user_id int,
+first_name varchar(255),
+last_name varchar(255),
+gender char,
+level varchar(255)
 );
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS dim_songs
 (
-song_id varchar(255) PRIMARY KEY,
-title varchar(255) NOT NULL,
-artist_id varchar(255) NOT NULL,
-year int NOT NULL,
-duration float NOT NULL
+song_id varchar(255),
+title varchar(255),
+artist_id varchar(255),
+year int,
+duration float
 );
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS dim_artists
 (
-artist_id varchar(255) NOT NULL,
-name varchar(255) NOT NULL,
-location varchar(255) NOT NULL,
-latitude real NOT NULL,
-longitude real NOT NULL
+artist_id varchar(255),
+name varchar(255),
+location varchar(255),
+latitude real,
+longitude real
 );
 """)
 
@@ -109,30 +110,43 @@ time_table_create = ("""
 CREATE TABLE IF NOT EXISTS dim_time
 (
 timestamp timestamp,
-hour int NOT NULL,
-day int NOT NULL,
-week int NOT NULL,
-month int NOT NULL,
-year int NOT NULL,
-weekday int NOT NULL
+hour int,
+day int,
+week int,
+month int,
+year int,
+weekday int
 );
 """)
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = \
+    (
+    """ 
+    COPY staging_events FROM {}
+     iam_role {} FORMAT AS JSON 'auto' COMPUPDATE OFF REGION 'us-west-2';
+    """
+).format(config.get("S3", "LOG_DATA"), config.get("IAM_ROLE", "ARN"))
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = (
+    """ 
+    COPY staging_songs FROM {}
+     iam_role {} FORMAT AS JSON 'auto' COMPUPDATE OFF REGION 'us-west-2';
+    """
+).format(config.get("S3", "SONG_DATA"), config.get("IAM_ROLE", "ARN"))
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
 """)
 
-user_table_insert = ("""
-""")
+user_table_insert = (
+    """
+    INSERT INTO dim_users
+    (SELECT userid from staging_events)
+    """
+)
 
 song_table_insert = ("""
 """)
@@ -148,4 +162,4 @@ time_table_insert = ("""
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, staging_events_table_create, staging_songs_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [user_table_insert, songplay_table_insert, song_table_insert, artist_table_insert, time_table_insert]
